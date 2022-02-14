a3140b87cfd83947fe383c7e2143880b78c8ae25
elaheerazob <ridwanelahee282@gmail.com>
2022-02-14 22:27:17 +0600

    add new

--- js/app.js
+++ js/app.js
@@ -1,16 +1,31 @@
-function updateProductNumber(){
-    const caseNumber =document.getElementById('case-number');
-    let caseTotal =caseNumber.value;
-    caseNumber.value=parseInt(caseTotal) + 1;
+function updateProductNumber(product,price,numberUp){
+    const prouctInpurt =document.getElementById(product + '-number');
+    let caseNumber =prouctInpurt.value;
+    if(numberUp){
+        caseNumber = parseInt(caseNumber) + 1;
+    }else if(caseNumber > 0){
+        caseNumber = parseInt(caseNumber) - 1 ;
+    }
+    prouctInpurt.value =caseNumber;
+
+    //Update total fild
+    const caseTotal = document.getElementById(product +'-total');
+    caseTotal.innerText =caseNumber * price;
 }
 
+//case 
 document.getElementById('case-plus').addEventListener('click',function(){
-    const caseNumber =document.getElementById('case-number');
-    let caseTotal =caseNumber.value;
-    caseNumber.value=parseInt(caseTotal) + 1;
+    updateProductNumber('case',59,true)
 })
 document.getElementById('case-minus').addEventListener('click',function(){
-    const caseNumber =document.getElementById('case-number');
-    let caseTotal =caseNumber.value;
-    caseNumber.value=parseInt(caseTotal) + 1;
+    updateProductNumber('case',59,false)
 })
+
+// phone
+document.getElementById('phone-plus').addEventListener('click',function(){
+    updateProductNumber('phone',1219,true)
+})
+document.getElementById('phone-minus').addEventListener('click',function(){
+    updateProductNumber('phone',1219,false)
+})
+
